cdc6e3c568e251ef163ba37b83f1a578adab11d6
onlykartika <kartika10112@gmail.com>
2024-02-26 12:35:55 +0700

    update

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ this is my documentation learning tallwind
 - Whatever the size like h1, h2, h3 it will be shown same. It will bw different if we set the class. Also, learn abaout height text decoration and import text
 - Customization margin. Learning how flex works and make something using width, heigh, padding and space between
 - Learning more deep about flex. Flex-row, coloumn, wrap and custom content in left center or right.
+- Grids. Learn how to customization row and coloumn with grid. Also learn how to antoher coloumn responsve
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="Parent">
-    <div>Header</div>
-    <div className="flex flex-col md:flex-row">
-    <div className="md:w-32 bg-blue-100">Main Content</div>
-    <div className="w-full bg-yellow-100">Sidebar</div>
+    <div className="h-screen text-white Parent bg-slate-900">
+      <div className="container mx-auto">
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-4">
+          <div className="p-6 rounded-lg bg-sky-500">First Color</div>
+          <div className="p-6 rounded-lg row-span-2 bg-sky-500">First Color</div>
+          <div className="col-span-2 p-6 rounded-lg bg-sky-500">First Color</div>
+          <div className="p-6 rounded-lg bg-sky-500">First Color</div>
+        </div>
+        <div className="m-12"></div>
+        <div className="grid grid-flow-col grid-rows-2 gap-4">
+          <div className="p-6 rounded-lg bg-sky-500">1</div>
+          <div className="p-6 rounded-lg bg-sky-500">2</div>
+          <div className="p-6 rounded-lg bg-sky-500">3</div>
+          <div className="p-6 rounded-lg bg-sky-500">4</div>
+          <div className="p-6 rounded-lg bg-sky-500">5</div>
+          <div className="p-6 rounded-lg bg-sky-500">6</div>
+        </div>
+        <div className="m-12"></div>
+        <div className="grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
+          <div className="col-span-2 p-6 rounded-lg bg-sky-500">1</div>
+          <div className="p-6 rounded-lg bg-sky-500">2</div>
+          <div className="p-6 rounded-lg bg-sky-500">3</div>
+          <div className="p-6 rounded-lg bg-sky-500">4</div>
+          <div className="p-6 rounded-lg bg-sky-500">5</div>
+          <div className="p-6 rounded-lg bg-sky-500">6</div>
+        </div>
+      </div>
     </div>
-    <div className="flex flex-col md:flex-row">
-      <div className="flex flex-wrap w-full">
-        <div className="w-32 grow">01</div>
-        <div className="w-32 grow">02</div>
-        <div className="w-32 grow">03</div>
-        <div className="w-32 grow">04</div>
-        <div className="w-32 grow">05</div>
-      </div>
-      <div className="flex justify-center">
-        <div>Sidebar</div>
-        <div>Main content</div>
-      </div>
-      <div className="flex justify-end">
-        <div>Sidebar</div>
-        <div>Main content</div>
-      </div>
-      <div className="flex justify-between">
-        <div className="bg-red-100">Sidebar</div>
-        <div className="bg-green-100">Main content</div>
-      </div>
-    </div>
-  </div>
   )
 }
